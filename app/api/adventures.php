b91068cdad0c889639ebd4a144213eca01b19b69
Benjamin Baxter <benjamin.g.baxter@gmail.com>
2015-08-19 18:58:38 -0400

    Cleaned up wording for small parts of the adventure, fixed broken links, updated adventure nav

--- app/api/adventures.php
+++ app/api/adventures.php
@@ -8,4 +8,10 @@ header('Access-Control-Allow-Origin: *');
 	"overview" : "Setup a Github account, Deploy First Tomcat app, JSP game.",
 	"date" : "9/3",
 	"id" : 1
+}, {
+	"title" : "Reprint Guessing Game",
+	"xp" : "50 xp",
+	"overview" : "Rewrite the guessing game with expressions and core jstl, setup gradle, create a simple user profile page.",
+	"date" : "9/10",
+	"id" : 2
 }]
