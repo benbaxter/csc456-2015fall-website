46aa38af49922a2eecb5764511d72c0d72f6a94a
Benjamin Baxter <benjamin.g.baxter@gmail.com>
2015-08-23 13:25:51 -0400

    Added android badge to overview, fixed typo in java class for first assignment

--- app/views/adventure1/Ch1Servlet.java
+++ app/views/adventure1/Ch1Servlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Ch1Serv extends HttpServlet {
+public class Ch1Servlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request,
 			HttpServletResponse response)
