14db175568fa31658696d457f784b7a117efa37d
Benjamin Baxter <benjamin.g.baxter@gmail.com>
2015-09-30 01:24:41 -0400

    Added more content for creating a user. Need to document the listener next

---
+++ app/views/adventure4/MysqlContextListener.java
@@ -0,0 +1,44 @@
+package edu.nku.j2ee;
+
+import java.sql.*;
+
+
+import javax.servlet.*;
+
+public class MysqlContextListener implements ServletContextListener {
+	Connection conn = null;
+	
+	public void contextInitialized (ServletContextEvent event){
+		try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (Exception ex) {
+				System.out.println("*****Error loading the driver*******");
+			}
+			
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/chat?user=root&password=");
+			ServletContext sc = event.getServletContext();
+			sc.setAttribute("dbconnection", conn);
+			System.out.println("*******set dbconnection attribute *******");
+			
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+	}
+	
+	public void contextDestroyed (ServletContextEvent event){
+		try{
+			conn.close();
+			conn = null;
+		}catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+}
